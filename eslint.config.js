9bf013f791d04523a3b199a3434350bf8dd82475
Karanyasuresh <143644155+Karanyasuresh@users.noreply.github.com>
2025-01-02 16:09:05 +0530

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    files: ['**/*.js'],
+    rules: {
+      'no-unused-vars': 'warn',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+    },
+  },
+];
